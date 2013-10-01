a80f3181cfa980b126679c7a0f1e8634dfa05c86
Casey Watson <casey.watson@readytalk.com>
2013-10-01 15:17:23 -0600

    improved parameter verification
    - fixed bug where extra shift would throw error
    - added tests for argument verification
    - fixed bug where bock would fail to initialize when it couldn't rm .bock

--- test/bock.sh
+++ test/bock.sh
@@ -13,17 +13,29 @@ fi
 
 echo -n "Testing if resetMocks clears out all mocks..."
 resetMocks thisCall
-
 verify thisCall 2>/dev/null && echo FAILED || echo PASSED
 
 echo -n "Testing if these mocks are carried into a sub-program..."
-
 source ./bock mv
 . test/testScript
+verify mv fakeFile1 fakeFile2 && echo PASSED || echo FAILED
 
-verify mv && echo PASSED || echo FAILED
-
-echo -n "Testing handling of repeated calls to resetMock..."
+echo -n "Testing handling of repeated resets..."
 source ./bock thisCall
 resetMocks thisCall
 resetMocks thisCall && echo PASSED || echo FAILED
+
+echo -n "Testing verification of missing arguments..."
+source ./bock thisCall
+thisCall
+verify thisCall one two three 2> /dev/null && echo FAILED || echo PASSED
+
+echo -n "Testing verification of extra arguments..."
+source ./bock thisCall
+thisCall one two three
+verify thisCall one two 2> /dev/null && echo FAILED || echo PASSED
+
+echo -n "Testing verification of matching arguments..."
+source ./bock thisCall
+thisCall one two three
+verify thisCall one two three && echo PASSED || echo FAILED
